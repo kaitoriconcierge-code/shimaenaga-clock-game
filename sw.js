89c85623e8ad5526f40469cfed3856f312cc6e81
kaitoriconcierge-code <kaitoriconcierge@gmail.com>
2026-09-21 07:01:29 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='shimaenaga-clock-v1';
+const ASSETS=['./','./index.html','./shimaenaga.png','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
